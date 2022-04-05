5d8e0fc950b8966650865bbf8d2162341a36ab38
kolya4359 <kolya4359@gmail.com>
2022-04-06 00:37:08 +0900

    update
    blog-frontend/logout

--- blog/blog-frontend/src/containers/common/HeaderContainer.js
+++ blog/blog-frontend/src/containers/common/HeaderContainer.js
@@ -1,11 +1,16 @@
 // Header 컴포넌트를 리덕스와 연결
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/common/Header';
+import { logout } from '../../modules/user';
 
 const HeaderContainer = () => {
   const { user } = useSelector(({ user }) => ({ user: user.user }));
-  return <Header user={user} />;
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(logout());
+  };
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderContainer;
