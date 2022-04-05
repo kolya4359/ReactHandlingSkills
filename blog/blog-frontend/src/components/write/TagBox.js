9a3fd3f8dadf76caaa18d1be745dad2dfc5190cc
kolya4359 <kolya4359@gmail.com>
2022-04-06 01:33:32 +0900

    update
    blog-frontend/write-tagbox

---
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -0,0 +1,135 @@
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const TagBoxBlock = styled.div`
+width: 100%;
+border-top: 1px solid ${palette.gray[2]};
+padding-top: 2rem;
+
+h4 {
+    color: ${palette.gray[8]}
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+}
+`;
+
+const TagForm = styled.form`
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  width: 256px;
+  border: 1px solid ${palette.gray[9]}; /* 스타일 초기화 */
+  input,
+  button {
+    outline: none;
+    border: none;
+    font-size: 1rem;
+  }
+
+  input {
+    padding: 0.5rem;
+    flex: 1;
+    min-width: 0;
+  }
+  button {
+    cursor: pointer;
+    padding-right: 1rem;
+    padding-left: 1rem;
+    border: none;
+    background: ${palette.gray[8]};
+    color: white;
+    font-weight: bold;
+    &:hover {
+      background: ${palette.gray[6]};
+    }
+  }
+`;
+
+const Tag = styled.div`
+  margin-right: 0.5rem;
+  color: ${palette.gray[6]};
+  cursor: pointer;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const TagListBlock = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+`;
+
+// React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링되도록 처리
+const TagItem = React.memo(({ tag, onRemove }) => (
+  <Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
+
+// React.memo를 사용하여 tags 값이 바뀔 때만 리렌더링되도록 처리
+const TagList = React.memo(({ tags, onRemove }) => (
+  <TagListBlock>
+    {tags.map((tag) => (
+      <TagItem key={tag} tag={tag} onRemove={onRemove} />
+    ))}
+  </TagListBlock>
+));
+
+const TagBox = () => {
+  const [input, setInput] = useState('');
+  const [localTags, setLocalTags] = useState([]);
+
+  const insertTag = useCallback(
+    (tag) => {
+      if (!tag) return; // 공백이라면 추가하지 않음
+      if (localTags.includes(tag)) return; // 이미 존재한다면 추가하지 않음
+      setLocalTags([...localTags, tag]);
+    },
+    [localTags],
+  );
+
+  const onRemove = useCallback(
+    (tag) => {
+      setLocalTags(localTags.filter((t) => t !== tag));
+    },
+    [localTags],
+  );
+
+  const onChange = useCallback((e) => {
+    setInput(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      insertTag(input.trim()); // 앞뒤 공백을 없앤 후 등록
+      setInput(''); // input 초기화
+    },
+    [input, insertTag],
+  );
+  return (
+    <TagBoxBlock>
+      <h4>태그</h4>
+      <TagForm onSubmit={onSubmit}>
+        <input
+          placeholder="태그를 입력하세요"
+          value={input}
+          onChange={onChange}
+        />
+        <button type="submit">추가</button>
+      </TagForm>
+      <TagList tags={localTags} onRemove={onRemove} />
+    </TagBoxBlock>
+  );
+};
+
+export default TagBox;
+
+/**
+ * TagBox 컴포넌트는 input이 바뀔 때와 태그 목록이 바뀔 때 리렌더링 된다.
+ * 그래서 최적화를 위해서 TagList와 TagItem 컴포넌트로 분리시켰다.
+ * 만약 컴포넌트를 분리하지 않으면 input 값이 바뀔 때 태그 목록도 리렌더링 될 것이다.
+ * 태그 목록이 리렌더링되면 태그 하나하나가 모두 리렌더링된다.
+ *
+ * React.memo를 사용해서 컴포넌트들을 감싸주면, 해당 컴포넌트가 받아 오는 props가 실제로
+ * 바뀌었을 때만 리렌더링해 준다.
+ */
--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,10 +1,14 @@
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButton';
 import Responsive from '../components/common/Responsive';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
