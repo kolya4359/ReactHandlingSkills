f6b2e25332f63e30cc3a5d0b82c94dad91f6e99a
kolya4359 <kolya4359@gmail.com>
2022-04-05 12:41:12 +0900

    update
    blog-frontend/login, register

--- blog/blog-frontend/src/components/auth/RegisterForm.js
+++ blog/blog-frontend/src/components/auth/RegisterForm.js
@@ -3,17 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
-const LoginForm = () => {
+const RegisterForm = () => {
   const dispatch = useDispatch();
   const { form } = useSelector(({ auth }) => ({
-    form: auth.login,
+    form: auth.register,
   }));
   // 인풋 변경 이벤트 핸들러
   const onChange = (e) => {
     const { value, name } = e.target;
     dispatch(
       changeField({
-        form: 'login',
+        form: 'register',
         key: name,
         value,
       }),
@@ -23,19 +23,17 @@ const LoginForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    // 구현 예정
+    // 구현예정
   };
 
   // 컴포넌트가 처음 렌더링될 때 form을 초기화함
-  // dispatch는 액션을 발생시킨다. 여기선 initializeForm 액션생성함수를 이용해 INITIALIZE_FORM 액션을 발생시킴
   useEffect(() => {
-    dispatch(initializeForm('login'));
+    dispatch(initializeForm('register'));
   }, [dispatch]);
-  // 이 작업을 하지 않으면, 로그인 페이지에서 값을 입력한 뒤 다른 페이지로 이동했다가 다시 돌아왔을 때 값이 유지된 상태로 보인다.
 
   return (
     <AuthForm
-      type="login"
+      type="register"
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
@@ -43,4 +41,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
